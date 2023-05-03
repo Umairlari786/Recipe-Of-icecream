ab237ff411d60ca7af900c83d8c4627b3d0c9967
Umairlari786 <uamir.lari1998@gmail.com>
2023-05-03 21:02:12 +0530

    adding first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+document.body.style.padding = "10px";
+
+
+function update(bgcolor,textcolor){
+
+    html.style.backgroundColor = bgcolor;
+    html.style.color =textcolor;
+}
+
+select.addEventListener("change", ()=>
+select.value ==="black"? update("black", "white"): update("white","black")
+);
